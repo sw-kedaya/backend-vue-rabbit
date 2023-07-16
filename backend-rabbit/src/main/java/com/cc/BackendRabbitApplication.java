94537a967ca6e7dd2796beda53f18bfe6a5779d1
achun <swwcc1103@163.com>
2023-07-16 20:01:07 +0800

    init

---
+++ backend-rabbit/src/main/java/com/cc/BackendRabbitApplication.java
@@ -0,0 +1,13 @@
+package com.cc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BackendRabbitApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BackendRabbitApplication.class, args);
+	}
+
+}
